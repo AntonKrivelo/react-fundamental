7a480d6b9466546c1a67ed78971bb633459c9e44
Anton Krivelo <krivelo2017@mail.ru>
2025-07-21 22:20:28 +0300

    hooks useState

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+
+  const [state, setState] = useState(0);
+
+  
+
+  const incr = () => {
+    
+    setState(state + 1)
+  }
+  const decr = () => {
+    if(state <= 0) {
+      return 0
+    }
+    setState(state - 1)
+    
+  }
+
+  return (
+    
+   <div className="App">
+        <h1>{state}</h1>
+        <button onClick={incr}>incr</button>
+        <button onClick={decr}>decr</button>
+    </div>
+  );
+}
+
+export default App;
