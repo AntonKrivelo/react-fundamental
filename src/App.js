ecb717d5b04855cf96d64a6e5e27caedf35f3d03
Anton Krivelo <krivelo2017@mail.ru>
2025-07-21 23:37:21 +0300

    add component PostList and edit props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import Post from './Components/Post';
+import PostList from './Components/PostList';
 import './styles/App.scss';
-import React, {useState} from 'react';
 
 
 function App() {
 
-  const [posts, setPosts] = useState([
-      {id:1, title:'JavaScript', description:'JavaScript - это язык программирования'}, 
-      {id:2, title:'Css', description:'CSS - это язык программирования'},
-      {id:3, title:'HTML', description:'HTML - это язык программирования'},
-  ])
 
   return (
     
     <div className="App">
-        {
-          posts.map(post => 
-           <Post post={post} key={post.id}/>
-        )}
+        <PostList title="Список постов 1"/>
     </div>
   );
 }
