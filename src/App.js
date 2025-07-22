ebce930f43d5ebba07b3695fb12f511a51ccbd47
Anton Krivelo <krivelo2017@mail.ru>
2025-07-22 22:38:39 +0300

    rendering code

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import PostForm from './Components/PostForm';
 function App() {
 
   const [posts, setPosts] = useState([]);
-  
+ 
   return (
     <div className="App">
       <h2 style={{textAlign: 'center', marginTop: '20px'}}>Введите необходимые данные для добавления поста:</h2>
         <PostForm  posts={posts} setPosts={setPosts} />
         {
-          posts.length > 0 ? <PostList posts={posts} setPosts={setPosts} title="Список постов 1"/> 
-          : <h2 style={{textAlign: 'center', marginTop: '20px', fontSize: '35px'}}> Нет постов...  </h2> 
-         
+          posts.length > 0 ? <PostList posts={posts} setPosts={setPosts} title='Постов 1' /> 
+          : <h2 style={{textAlign: 'center', marginTop: '20px', fontSize: '35px'}}> Посты не найдены...  </h2> 
         }
     </div>
   );
