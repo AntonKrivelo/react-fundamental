14b208d1c856d903451b1bedcec26a840ce41adc
Anton Krivelo <krivelo2017@mail.ru>
2025-07-22 15:35:19 +0300

    Создал получение значения в логах из инпута при клике на кнопку создать пост

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import PostList from './Components/PostList';
 import MyButton from './Components/UI/button/MyButton';
 import MyInput from './Components/UI/input/MyInput';
+import React,{useState} from 'react';
 import './styles/App.scss';
 
 
 function App() {
 
-  const addNewPost = () => {
-    
+  
+  const [posts, setPosts] = useState([
+      {id:1, title:'JavaScript', description:'JavaScript - это язык программирования'}, 
+      {id:2, title:'Css', description:'CSS - это язык программирования'},
+      {id:3, title:'HTML', description:'HTML - это язык программирования'},
+  ])
+  
+  const [title, setTitle] = useState('sadsad')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
   }
 
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="Заголовок поста" />
+        <MyInput value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Заголовок поста" />
         <MyInput type="text" placeholder="Описание поста" />
-        <MyButton onClick={addNewPost} disabled>Создать пост</MyButton>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
-        <PostList title="Список постов 1"/>
+        <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,18 +1,11 @@
-import React,{useState} from 'react'
 import Post from './Post'
 
 
-const PostList = (props) => {
-
-  const [posts, setPosts] = useState([
-      {id:1, title:'JavaScript', description:'JavaScript - это язык программирования'}, 
-      {id:2, title:'Css', description:'CSS - это язык программирования'},
-      {id:3, title:'HTML', description:'HTML - это язык программирования'},
-  ])
+const PostList = ({posts, title}) => {
 
   return (
     <div>
-       <h1 style={{textAlign: 'center'}}>{props.title}</h1>
+       <h1 style={{textAlign: 'center'}}>{title}</h1>
         {
           posts.map(post => 
            <Post post={post} key={post.id}/>
