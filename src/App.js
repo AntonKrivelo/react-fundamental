cba4fd437753a8ed666e43166d03e89cf34e2e77
Anton Krivelo <krivelo2017@mail.ru>
2025-07-24 19:39:15 +0300

    Добавил сортировку постов

--- src/App.js
+++ src/App.js
@@ -9,12 +9,25 @@ function App() {
 
   const [posts, setPosts] = useState([]);
  
+  const [selectedSort, setSelectedSort] = useState('');
+
+  const sortPost = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  }
+
   return (
     <div className="App">
       <h2 style={{textAlign: 'center', marginTop: '20px'}}>Введите необходимые данные для добавления поста:</h2>
         <PostForm  posts={posts} setPosts={setPosts} />
         <hr style={{marginTop: '30px'}}></hr>
-        <MySelect />
+        <MySelect 
+            value={selectedSort}
+            onChange={sortPost}
+            defaultValue="Сортировка" options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'description', name: 'По описанию'}
+        ]}/>
         {
           posts.length > 0 ? <PostList posts={posts} setPosts={setPosts} title='Постов 1' /> 
           : <h2 style={{textAlign: 'center', marginTop: '20px', fontSize: '35px'}}> Посты не найдены...  </h2> 
--- src/Components/UI/select/MySelect.jsx
+++ src/Components/UI/select/MySelect.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const MySelect = () => {
+const MySelect = ({options, defaultValue, value, onChange}) => {
   return (
     <div style={{marginTop: '20px'}}>
-        <select>
-            <option value='value'>По названию</option>
-            <option value='value'>По описанию</option>
+        <select 
+          value={value}
+          onChange={event => onChange(event.target.value)}
+        >
+            <option disabled value=''>{defaultValue}</option>
+            {
+                options.map(option => 
+                   <option key={option.value} value={option.value}>
+                        {option.name}
+                   </option> 
+                )
+            }
         </select>
     </div>
   )
